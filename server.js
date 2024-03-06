4b0ae0c835b1828c8e2757e57880895620470a5e
John Turner Dick <JD@Johns-MacBook-Pro-2.local>
2024-03-06 17:20:23 +0000

    updated cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "https://sunny-app.netlify.app" }));
 
 app.use(express.json());
 
